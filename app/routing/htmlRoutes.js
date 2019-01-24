d4c6a97ea00b898309a403c2468af75ac88b14f5
John <jlev2282@gmail.com>
2019-01-23 21:58:10 -0500

    coded htmlRoutes.js file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,25 @@
+//include path package to get correct file path for our html
+var path = require("path");
+
+//ROUTING
+
+module.exports = function(app) {
+
+    //HTML GET Requests
+
+    //route to take user to contact request form
+    app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/contact.html"));
+    });
+
+    //route to take user to survey guaging their interest in purchasing art
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    //catch all route to return to home if nothing else matches
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+
+};
